351edb2c411c82b58c7a78d34b528d4cbd075478
Thomas Duffy <tduffy000@citymail.cuny.edu>
2021-09-05 19:43:37 -0400

    the simplest deployment script ever

---
+++ model/deploy_latest_model.sh
@@ -0,0 +1,14 @@
+SRC_ROOT=./artifacts
+MODEL_DIR=$(ls ${SRC_ROOT} | sort)
+MODEL_ASSET=mobile_model.pt
+
+MODEL_SRC=$SRC_ROOT/$MODEL_DIR/$MODEL_ASSET
+
+echo "deploying $SRC_ROOT/$MODEL_DIR/$MODEL_ASSET ..."
+
+TGT_DIR=../android/app/src/main/assets
+rm -rf $TGT_DIR
+mkdir -p $TGT_DIR
+
+cp $MODEL_SRC $TGT_DIR/
+echo "deployed model version: $MODEL_DIR"
