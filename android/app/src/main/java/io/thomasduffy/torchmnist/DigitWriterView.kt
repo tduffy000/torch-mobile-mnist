e39495cc091ae714e5c74641079f1337a56b9e95
Thomas Duffy <tduffy000@citymail.cuny.edu>
2021-09-06 10:18:19 -0400

    render a constrained layout

--- android/app/src/main/java/io/thomasduffy/torchmnist/DigitWriterView.kt
+++ android/app/src/main/java/io/thomasduffy/torchmnist/DigitWriterView.kt
@@ -2,6 +2,7 @@ package io.thomasduffy.torchmnist
 
 import android.content.Context
 import android.graphics.*
+import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewConfiguration
@@ -9,12 +10,10 @@ import androidx.core.content.res.ResourcesCompat
 
 private const val STROKE_WIDTH = 12f
 
-class DigitWriterView(ctx: Context): View(ctx) {
+class DigitWriterView(ctx: Context, attrs: AttributeSet): View(ctx, attrs) {
 
     private lateinit var extraCanvas: Canvas
     private lateinit var extraBitmap: Bitmap
-
-    private lateinit var frame: Rect
 
     private var motionTouchEventX = 0f
     private var motionTouchEventY = 0f
@@ -27,8 +26,8 @@ class DigitWriterView(ctx: Context): View(ctx) {
     private val bgColor = ResourcesCompat.getColor(resources, R.color.purple_200, null)
     private val drawColor = ResourcesCompat.getColor(resources, R.color.black, null)
 
-    private var allPoints: MutableList<MutableList<Pair<Float, Float>>> = ArrayList()
-    private var pointSegment: MutableList<Pair<Float, Float>> = ArrayList()
+//    private var allPoints: MutableList<MutableList<Pair<Float, Float>>> = ArrayList()
+//    private var pointSegment: MutableList<Pair<Float, Float>> = ArrayList()
 
     private val paint = Paint().apply {
         color = drawColor
@@ -58,59 +57,59 @@ class DigitWriterView(ctx: Context): View(ctx) {
     }
 
     // finger presses down
-    private fun touchStart() {
-        path.reset()
-        path.moveTo(motionTouchEventX, motionTouchEventY)
-        currentX = motionTouchEventX
-        currentY = motionTouchEventY
-    }
-
-    // finger moves
-    private fun touchMove() {
-        val dx = Math.abs(motionTouchEventX - currentX)
-        val dy = Math.abs(motionTouchEventY - currentY)
-        if (dx >= touchTolerance || dy >= touchTolerance) {
-            // QuadTo() adds a quadratic bezier from the last point,
-            // approaching control point (x1,y1), and ending at (x2,y2).
-            path.quadTo(currentX, currentY, (motionTouchEventX + currentX) / 2, (motionTouchEventY + currentY) / 2)
-            currentX = motionTouchEventX
-            currentY = motionTouchEventY
-
-            // add point to current segment being drawn
-            pointSegment.add(Pair(currentX, currentY))
-
-            extraCanvas.drawPath(path, paint)
-        }
-        invalidate()
-    }
-
-    // finger comes up
-    private fun touchUp() {
-        // add the drawn segment to the set of drawn segments (allPoints)
-        allPoints.add(pointSegment)
-        pointSegment.clear()
-        path.reset()
-    }
-
-    fun getPoints(): MutableList<MutableList<Pair<Float, Float>>> {
-        return allPoints
-    }
-
-    fun clearPoints() {
-        allPoints.clear()
-    }
-
-    override fun onTouchEvent(event: MotionEvent): Boolean {
-        motionTouchEventX = event.x
-        motionTouchEventY = event.y
-
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> touchStart()
-            MotionEvent.ACTION_MOVE -> touchMove()
-            MotionEvent.ACTION_UP -> touchUp()
-        }
-
-        return true
-    }
+//    private fun touchStart() {
+//        path.reset()
+//        path.moveTo(motionTouchEventX, motionTouchEventY)
+//        currentX = motionTouchEventX
+//        currentY = motionTouchEventY
+//    }
+//
+//    // finger moves
+//    private fun touchMove() {
+//        val dx = Math.abs(motionTouchEventX - currentX)
+//        val dy = Math.abs(motionTouchEventY - currentY)
+//        if (dx >= touchTolerance || dy >= touchTolerance) {
+//            // QuadTo() adds a quadratic bezier from the last point,
+//            // approaching control point (x1,y1), and ending at (x2,y2).
+//            path.quadTo(currentX, currentY, (motionTouchEventX + currentX) / 2, (motionTouchEventY + currentY) / 2)
+//            currentX = motionTouchEventX
+//            currentY = motionTouchEventY
+//
+//            // add point to current segment being drawn
+////            pointSegment.add(Pair(currentX, currentY))
+//
+//            extraCanvas.drawPath(path, paint)
+//        }
+//        invalidate()
+//    }
+//
+//    // finger comes up
+//    private fun touchUp() {
+//        // add the drawn segment to the set of drawn segments (allPoints)
+////        allPoints.add(pointSegment)
+////        pointSegment.clear()
+//        path.reset()
+//    }
+//
+//    fun getPoints(): MutableList<MutableList<Pair<Float, Float>>> {
+//        return allPoints
+//    }
+//
+//    fun clearPoints() {
+//        allPoints.clear()
+//    }
+//
+//    override fun onTouchEvent(event: MotionEvent): Boolean {
+//        motionTouchEventX = event.x
+//        motionTouchEventY = event.y
+//
+//        when (event.action) {
+//            MotionEvent.ACTION_DOWN -> touchStart()
+//            MotionEvent.ACTION_MOVE -> touchMove()
+//            MotionEvent.ACTION_UP -> touchUp()
+//        }
+//
+//        return true
+//    }
 
 }
--- android/app/src/main/java/io/thomasduffy/torchmnist/MainActivity.kt
+++ android/app/src/main/java/io/thomasduffy/torchmnist/MainActivity.kt
@@ -2,14 +2,22 @@ package io.thomasduffy.torchmnist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val digitView = DigitWriterView(this)
-        digitView.contentDescription = getString(R.string.canvasContentDescription)
+        setContentView(R.layout.activity_main)
 
-        setContentView(digitView)
+//        val predictionView = findViewById<TextView>(R.id.prediction)
+        val drawView = findViewById<DigitWriterView>(R.id.digitWriterView)
+//        val predictButton = findViewById<Button>(R.id.predictButton)
+//        val clearButton = findViewById<Button>(R.id.clearButton)
+
     }
 }
